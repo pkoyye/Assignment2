b71009d965662cf0612d7966a766438ea2257b46
19287 <19287@LAPTOP-BG8J2JLV>
2020-10-03 18:47:11 -0700

    Assignment2

---
+++ Assignment2/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,29 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	  //Instance variables are going to be defined here
+	  private int total;
+	  
+	  
+	  //This is a default constructor called AddingMachine
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  //This is a method to get the total value
+	  public int getTotal () {
+	    return 0;
+	  }
+	  //This is a method to add the int variable value into total
+	  public void add (int value) {
+	  }
+      //This is a method to subtract the int variable value from total
+	  public void subtract (int value) {
+	  }
+      //This is a toString method that returns the string representation
+	  public String toString () {
+	    return "";
+	  }
+      //This is a method to clear the memory
+	  public void clear() {
+	  }
+	}
